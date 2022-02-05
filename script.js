b502b59a9dbb2815b738ba87cebe369757b7cea1
Collus Watson <cklwatsonjr@gmail.com>
2022-02-05 09:01:03 -0500

    html/ added divs and containers to hold infromation.. js/ added fucntions to modify form data

--- script.js
+++ script.js
@@ -1,7 +1,14 @@
 const wageForm = () => document.getElementById('submitWage'); //grabs form by i.d.
 const inputWage = () => document.getElementById('netPay'); //grabs wage
 const inputRate = () => document.getElementById('wageRate') //grabs selected rate
+const budgetAmount = document.getElementById("budgetAmount");
+const balanceAmount = document.getElementById("balanceAmount");
 
+const hourlyAmount = document.getElementById('hourly')
+const weeklyAmount = document.getElementById('weekly')
+const biweeklyAmount = document.getElementById('biweekly')
+const monthlyAmount = document.getElementById('monthly')
+const yearlyAmount = document.getElementById('yearly')
 
 wageForm().addEventListener('submit', (event) => {
     event.preventDefault();
@@ -10,13 +17,47 @@ wageForm().addEventListener('submit', (event) => {
     makeBudget(startWage, selectedRate)
 })
 
-function makeBudget(wage, rate){
-    console.log( wage, rate)
-    if(rate == 'hr'){
-        console.log(wage + ' per hour')
-    }else if(rate == 'yr'){
-        console.log(wage + ' yearly')
+function makeBudget(wage, rate) {
+    let hr; //hourly
+    let dy; //daily
+    let wk; //weekly
+    let bw; //biWeekly
+    let mo; //monthly
+    let yr; //yearly
+    switch (rate) {
+        case 'hr':
+            hr = (wage)
+            dy = ((wage * 40) / 7)
+            wk = (wage * 40)
+            bw = ((((wage * 40) * 52) / 12) / 2)
+            mo = (((wage * 40) * 52) / 12)
+            yr = ((wage * 40) * 52)
+            break;
+        case 'yr':
+            hr = ((wage / 52) / 40)
+            dy = ((wage / 52) / 7)
+            wk = (wage / 52)
+            bw = ((wage / 12) / 2)
+            mo = (wage / 12)
+            yr = wage
+            break;
     }
+    if (!wage) {
+        console.log('empty')
+    } else {
+        hourlyAmount.innerHTML = hr
+        weeklyAmount.innerHTML = wk
+        biweeklyAmount.innerHTML = bw
+        monthlyAmount.innerHTML = mo
+        yearlyAmount.innerHTML = yr
+    }
+    // console.log(hr)
+    // console.log(dy)
+    // console.log(wk)
+    // console.log(bw)
+    // console.log(mo)
+    // console.log(yr)
+    // console.log(balance)
 }
 
 
